2fabbd88c5c0fb4c0923b0789d76d01f0fc529cc
kimocoder <christian@aircrack-ng.org>
2025-11-24 10:39:12 +0100

    Update merge tool

--- merge_upstream.sh
+++ merge_upstream.sh
@@ -4,13 +4,13 @@
 #git pull msm-4.19 $1
 
 # Techpacks
-git subtree pull --prefix=techpack/audio audio $1
+#git subtree pull --prefix=techpack/audio audio $1
 #git subtree pull --prefix=techpack/camera camera $1
-git subtree pull --prefix=techpack/dataipa dataipa $1
-git subtree pull --prefix=techpack/datarmnet datarmnet $1
-git subtree pull --prefix=techpack/datarmnet-ext datarmnet-ext $1
-git subtree pull --prefix=techpack/display display $1
-git subtree pull --prefix=techpack/video video $1
+#git subtree pull --prefix=techpack/dataipa dataipa $1
+#git subtree pull --prefix=techpack/datarmnet datarmnet $1
+#git subtree pull --prefix=techpack/datarmnet-ext datarmnet-ext $1
+#git subtree pull --prefix=techpack/display display $1
+#git subtree pull --prefix=techpack/video video $1
 
 # Wi-Fi
 git subtree pull --prefix=drivers/staging/fw-api fw-api $1
